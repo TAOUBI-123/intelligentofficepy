799902c2c3d31a948fa3debb2dcd0dedd630fac4
jtaoubi <taoubijihad6@gmail.com>
2025-12-18 14:12:51 +0100

    red phase s1

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -5,11 +5,14 @@ import mock.GPIO as GPIO
 from mock.SDL_DS3231 import SDL_DS3231
 from mock.adafruit_veml7700 import VEML7700
 from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
-
+import unittest
+from unittest.mock import patch, MagicMock
 
 class TestIntelligentOffice(unittest.TestCase):
 
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    def test_check_quadrant_found_worker(self, mock_gpio: Mock ):
+        mock_gpio_input.return_value = True
+        office = IntelligentOffice()
+        result = office.check_quadrant_occupancy(11)
+        self.assertTrue(result)
